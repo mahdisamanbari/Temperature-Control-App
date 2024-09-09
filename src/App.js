416d12e8f959c39621e233dccbd213c7d5aa0899
Mahdi Samanbari <mahdisamanbari.it@gmail.com>
2024-09-09 20:14:55 +0330

    temperature color added

--- src/App.js
+++ src/App.js
@@ -2,15 +2,33 @@ import React,{useState} from 'react';
 
 const App = () => {
   const [temperatureValue,setTemperatureValue] = useState(10);
+  const [temperatureColor,settemperatureColor] = useState('cold');
+
+  const addTemp=() => {
+    const newTemperature=temperatureValue + 1
+    if (newTemperature ===30) return;
+    if (newTemperature >= 15){
+      settemperatureColor('hot')
+    }
+    setTemperatureValue(newTemperature)
+  };
+  const removeTemp=() => {
+    const newTemperature=temperatureValue - 1
+    if (newTemperature ===0) return;
+    if (newTemperature < 15){
+      settemperatureColor('cold')
+    }
+    setTemperatureValue(newTemperature)
+  };
   
   return (
     <div className='app-container'>
       <div className='temperature-display-container'>
-        <div className='temperature-display'>{temperatureValue}ºc</div>
+        <div className={`temperature-display ${temperatureColor}`}>{temperatureValue}ºc</div>
       </div>
       <div className='button-container'>
-        <button onClick={()=>setTemperatureValue(temperatureValue +1)}>+</button>
-        <button onClick={()=>setTemperatureValue(temperatureValue -1)}>-</button>
+        <button onClick={()=>addTemp()}>+</button>
+        <button onClick={()=>removeTemp()}>-</button>
       </div>
       
     </div>
